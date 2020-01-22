86fbc91dc6a241083164b72a06b6c4eed002d41c
Manoel Henrique <henriqe115@gmail.com>
2020-01-21 23:58:20 -0300

    correção para inserir a cor obtida da API em cada shape criado na viewHolder

--- app/src/main/java/com/manoelh/cores/adapter/ListaDeCoresAdapter.kt
+++ app/src/main/java/com/manoelh/cores/adapter/ListaDeCoresAdapter.kt
@@ -11,7 +11,7 @@ class ListaDeCoresAdapter(private val coresEmHexadecimal: List<String>) : Recycl
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoresViewHolder {
         val inflate = LayoutInflater.from(parent.context)
         val view = inflate.inflate(R.layout.lista_de_cores , parent, false)
-        return CoresViewHolder(view, parent.context)
+        return CoresViewHolder(view)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/manoelh/cores/viewHolder/CoresViewHolder.kt
+++ app/src/main/java/com/manoelh/cores/viewHolder/CoresViewHolder.kt
@@ -1,17 +1,19 @@
 package com.manoelh.cores.viewHolder
 
-import android.content.Context
 import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.view.View
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.manoelh.cores.R
 
-class CoresViewHolder(itemView: View, context: Context) : RecyclerView.ViewHolder(itemView){
+
+class CoresViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
     private val retangulo = itemView.findViewById<ImageView>(R.id.imageViewCor)
 
     fun vincularCores(corDaLista: String){
-        retangulo.setBackgroundColor(Color.parseColor(corDaLista))
+        val shape: GradientDrawable =  retangulo.drawable as GradientDrawable
+        shape.setColor(Color.parseColor(corDaLista))
     }
 }
